9cd9037053ad009bd8d35f81fec2603d005cebdb
LuciaSegovia <lsegoviarevilla@gmail.com>
2021-06-09 09:45:23 +0100

    initial test to add metadata to GeoNetwork

--- GeoNetwork_API.R
+++ GeoNetwork_API.R
@@ -7,19 +7,24 @@ install.packages("usethis")
 
 library(devtools)
 
+#dzvoti
 
 
 ###############-----------------------------#################
 
+library(geometa)
 library(geonapi)
 
 #Setting up GeoNetwork API
 
+user <- "your-user-name"
+pass <- "your-password"
+
 GN <- GNManager$new(
   url = "https://metadata.micronutrient.support/geonetwork", #base URL of the Geonetwork
   version = "3.10.4.0",
-  user = "lucia", pwd = "02/Jun/1987", #credentials (ignore if you want to connect as anonymous user for metadata reading)
-  logger = NULL
+  user = user, pwd = pass, #credentials (ignore if you want to connect as anonymous user for metadata reading)
+  logger = "DEBUG"
 )
 
 
@@ -27,11 +32,7 @@ GN <- GNManager$new(
 
 mdfile <- system.file("extdata/examples", "metadata.xml", package = "geonapi")
 
-mdfile1 <- "C:/Users/LuciaSegoviaDeLaRevi/OneDrive - London School of Hygiene and Tropical Medicine/MAPS/03_open-science/R_MAPS_open-science/data_metadata/ihs4_metadata.xml"
-
 created = GN$insertMetadata(file = mdfile, group = "1", category = "datasets")
-
-created1 = GN$insertMetadata(file = mdfile1, group = "1", category = "datasets")
 
 config <- GNPrivConfiguration$new()
 
@@ -40,7 +41,7 @@ config$setPrivileges("all", c("view","dynamic","featured"))
 GN$setPrivConfiguration(id = created, config = config)
 
 
-id <- "96c0b0e3-dfec-4a29-ac06-49ae40d9e960" 
+id <- "d38a8576-ecf1-429d-bc4b-6db346dc3c6e" 
 
-md <- GN$getMetadataByUUID(id)
+test <- GN$getMetadataByUUID(id)
 
